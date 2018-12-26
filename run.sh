08758e5586bb48ae26a02aac54c20e9796fdf4fc
Peter Boráros <pborky@pborky.sk>
2018-12-26 18:38:27 +0100

    run daemonized named contianer

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run --rm -it --device /dev/vchiq -p 127.0.0.1:8080:8080  pborky/mjpeg-streamer
+docker run --name mjpeg-streamer -d --restart unless-stopped -it --device /dev/vchiq -p 127.0.0.1:8080:8080  pborky/mjpeg-streamer $@
+
 
