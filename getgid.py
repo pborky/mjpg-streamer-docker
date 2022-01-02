adeb187514ec88587fd950b1bb40c55d62edd962
pBorky <pborky@pborky.sk>
2022-01-02 02:13:05 +0000

    obtain gids from device list

--- getgid.py
+++ getgid.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
-import grp, sys
-
+import grp, sys, os, yaml
+from operator import itemgetter
+from itertools import chain
 
 def getgid(g):
+    ''' get gid for group name'''
     try:
         return grp.getgrnam(g).gr_gid
     except KeyError:
         pass
 
+def get_file_gid(fn):
+    ''' get gid for file ame'''
+    try:
+        return os.stat(fn).st_gid
+    except:
+        pass
 
-required_groups = ["video"]
-gids = filter(None, map(getgid, required_groups + sys.argv))
+with open('docker-compose.yml', 'r') as f: 
+    compose = yaml.safe_load(f)
 
-print("gids=%s" % ",".join(map(str, gids)))
+# get device list for each service in composefile
+services = compose['services'].values()
+devices = list(chain(*map(itemgetter('devices'), services)))
+
+# obtain gids for devices and custom ones provided on commanline
+gids = chain(map(get_file_gid, devices), filter(None, map(getgid, sys.argv)))
+
+print("gids=%s" % ",".join(map(str, set(gids))))
